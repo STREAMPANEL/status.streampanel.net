5e462ede22a24d47735dbdb7b91c0c78d13548f5
Sven <scysys@outlook.com>
2022-12-28 00:58:53 +0100

    Create privacy.js

---
+++ assets/privacy.js
@@ -0,0 +1,6 @@
+<script>
+var _mtm = window._mtm = window._mtm || [];
+_mtm.push({'mtm.startTime': (new Date().getTime()), 'event': 'mtm.Start'});
+var d=document, g=d.createElement('script'), s=d.getElementsByTagName('script')[0];
+g.async=true; g.src='https://pw.streampanel.net/js/container_KVNJAR2p.js'; s.parentNode.insertBefore(g,s);
+</script>
